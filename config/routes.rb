134d7390b9a29da298a25958fc9e61229fdfda42
Eisuke Sato <taro.kirameki@techacademy.jp>
2020-08-15 11:28:47 +0000

    お問い合わせページ練習

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:new, :create]
   resources :tasks
+  
+  get "contacts", to: "contacts#new"
+  post "contacts", to: "contacts#create"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_25_154949) do
+ActiveRecord::Schema.define(version: 2020_08_15_112438) do
+
+  create_table "contacts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
